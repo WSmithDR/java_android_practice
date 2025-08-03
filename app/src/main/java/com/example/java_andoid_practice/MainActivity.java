d7d19a6a01f4275450dbee3ec5b06e158ac0d964
WSmithDR <wsduenas@espol.edu.ec>
2025-08-03 17:27:00 -0500

    created template

--- app/src/main/java/com/example/java_andoid_practice/MainActivity.java
+++ app/src/main/java/com/example/java_andoid_practice/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.java_andoid_practice;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,61 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editTextName;
+    private EditText editTextAge;
+    private RadioGroup grupoGeneros;
+    private Spinner spRol;
+    private CheckBox chkTerminos;
+    private CheckBox chkInformacion;
+
+    public EditText getEditTextName() {
+        return editTextName;
+    }
+
+    public void setEditTextName(EditText editTextName) {
+        this.editTextName = editTextName;
+    }
+
+    public EditText getEditTextAge() {
+        return editTextAge;
+    }
+
+    public void setEditTextAge(EditText editTextAge) {
+        this.editTextAge = editTextAge;
+    }
+
+    public RadioGroup getGrupoGeneros() {
+        return grupoGeneros;
+    }
+
+    public void setGrupoGeneros(RadioGroup grupoGeneros) {
+        this.grupoGeneros = grupoGeneros;
+    }
+
+    public Spinner getSpRol() {
+        return spRol;
+    }
+
+    public void setSpRol(Spinner spRol) {
+        this.spRol = spRol;
+    }
+
+    public CheckBox getChkTerminos() {
+        return chkTerminos;
+    }
+
+    public void setChkTerminos(CheckBox chkTerminos) {
+        this.chkTerminos = chkTerminos;
+    }
+
+    public CheckBox getChkInformacion() {
+        return chkInformacion;
+    }
+
+    public void setChkInformacion(CheckBox chkInformacion) {
+        this.chkInformacion = chkInformacion;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
