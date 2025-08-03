fb294934ae010196396a42e84ca47627d35083cb
WSmithDR <wsduenas@espol.edu.ec>
2025-08-03 17:53:39 -0500

    created template

--- app/src/main/java/com/example/java_andoid_practice/MainActivity.java
+++ app/src/main/java/com/example/java_andoid_practice/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.java_andoid_practice;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -124,5 +125,17 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter =
                 ArrayAdapter.createFromResource(this,R.array.roles_array, android.R.layout.simple_spinner_item);
         spRol.setAdapter(adapter);
+    }
+
+    public void crearCliente(View view){
+
+    }
+
+    public void borrar(View view){
+
+    }
+
+    public void mostrarClientes(View view){
+
     }
 }
