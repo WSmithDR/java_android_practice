d9dca4adbdd7dfc5899859325b16c2f582616dd7
WSmithDR <wsduenas@espol.edu.ec>
2025-08-10 11:24:58 -0500

    created template

--- app/src/main/java/com/example/u6_c4_visor_imagenes1_inicio_clase/MainActivity.java
+++ app/src/main/java/com/example/u6_c4_visor_imagenes1_inicio_clase/MainActivity.java
@@ -16,16 +16,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button btnCargarImagen;
     private ImageView imagenSeleccionada;
     private ActivityResultLauncher<Intent> imagePickerLauncher;
+
+    private Button btnGuardarImagen;
 
     private Uri imageUriSeleccionada;
     @Override
@@ -40,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         btnCargarImagen = findViewById(R.id.btnCargarImagen);
+        btnGuardarImagen = findViewById(R.id.btnGuardar);
         imagenSeleccionada = findViewById(R.id.imagenSeleccionada);
         imagePickerLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
@@ -75,6 +80,44 @@ public class MainActivity extends AppCompatActivity {
                         Intent intent = new Intent(Intent.ACTION_PICK);
                         intent.setType("image/*");
                         imagePickerLauncher.launch(intent);
+                    }
+                }
+        );
+
+
+        btnGuardarImagen.setOnClickListener(
+                new View.OnClickListener(
+
+                ) {
+                    @Override
+                    public void onClick(View v) {
+                        if(imageUriSeleccionada!=null){
+                            String uri = imageUriSeleccionada.toString();
+                            String linea = "imagen,"+uri+"\n";
+                            try{
+                                BufferedWriter bw = new BufferedWriter(
+                                        new OutputStreamWriter(
+                                                openFileOutput("eventodata.txt",MODE_APPEND)
+                                        )
+                                );
+                                bw.write(linea);
+                                bw.close();
+                                Toast.makeText(MainActivity.this,"Evento guardado",Toast.LENGTH_SHORT).show();
+                            }catch(IOException e){
+                                    e.printStackTrace();
+                                    Toast.makeText(
+                                            MainActivity.this,
+                                            "Error al guardar",
+                                            Toast.LENGTH_SHORT
+                                    ).show();
+                            }
+                        }else{
+                            Toast.makeText(
+                                    MainActivity.this,
+                                    "Debe seleccionar una imagen antes de guardar",
+                                    Toast.LENGTH_SHORT
+                            ).show();
+                        }
                     }
                 }
         );
