bd4fc755056ec7930a7e69b412749153ba9741a9
WSmithDR <wsduenas@espol.edu.ec>
2025-08-10 16:26:52 -0500

    created template

---
+++ app/src/main/java/com/example/u6_c4_visor_imagenes1_inicio_clase/Visor.java
@@ -0,0 +1,24 @@
+package com.example.u6_c4_visor_imagenes1_inicio_clase;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Visor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_visor);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
