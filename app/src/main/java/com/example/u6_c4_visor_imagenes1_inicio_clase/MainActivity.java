6336c872d40197983a372ecc1ccfbb4493f20b64
WSmithDR <wsduenas@espol.edu.ec>
2025-08-10 08:41:24 -0500

    created template

--- app/src/main/java/com/example/u6_c4_visor_imagenes1_inicio_clase/MainActivity.java
+++ app/src/main/java/com/example/u6_c4_visor_imagenes1_inicio_clase/MainActivity.java
@@ -2,38 +2,15 @@ package com.example.u6_c4_visor_imagenes1_inicio_clase;
 
 import android.os.Bundle;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
-
 import androidx.activity.EdgeToEdge;
-import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStreamWriter;
 
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnCargarImagen;
-    private ImageView imagenSeleccionada;
-    private static final int REQUEST_IMAGE_PICK = 100;
-    private ActivityResultLauncher<Intent> imagePickerLauncher;
-    private Button btnGuardar;
-    private Button btnMostrarListado;
-    private Uri imageUriSeleccionada;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,12 +21,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
-        btnCargarImagen = findViewById(R.id.btnCargarImagen);
-        imagenSeleccionada = findViewById(R.id.imagenSeleccionada);
-        btnGuardar = findViewById(R.id.btnGuardar);
-        btnMostrarListado=findViewById(R.id.btnMostrarListado);
 
 
 
